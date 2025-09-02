8ef6e14ebce671450dbe6dc7fe6f26697c261804
aayanksinghai <aayanksinghai02@gmail.com>
2025-09-02 15:42:20 +0530

    HL2 P14 P15

---
+++ Hands-On-List2-Files/P14/14.c
@@ -0,0 +1,36 @@
+/*
+===========================================================================================================================
+Name : 14.c
+Author : Aayank Singhai
+Description : Write a simple program to create a pipe, write to the pipe, read from pipe and display on the monitor.
+Date: 2nd Sept, 2025.
+===========================================================================================================================
+*/
+
+
+#include  <stdio.h>
+#include  <unistd.h>
+#include  <string.h>
+
+int main(void){
+    int fd[2];
+    char buff[100];
+
+    char *message = "Hello My Name is Aayank\n";
+    pipe(fd);
+
+    write(fd[1], message, strlen(message));
+    int bytes_read = read(fd[0], buff, sizeof(buff));
+    buff[bytes_read] = '\0';
+    printf("Read from Pipe: %s\n", buff);
+    return 0;
+}
+
+
+/*
+OUTPUT
+===========================================================================================================================
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P14$ ./a.out
+Read from Pipe: Hello My Name is Aayank
+===========================================================================================================================
+*/
---
+++ Hands-On-List2-Files/P15/15.c
@@ -0,0 +1,40 @@
+/*
+===========================================================================================================================
+Name : 15.c
+Author : Aayank Singhai
+Description : Write a simple program to send some data from parent to the child process.
+Date: 2nd Sept, 2025.
+===========================================================================================================================
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void){
+    int fd[2];
+    char buff[100];
+
+    pipe(fd);
+
+    if(fork()){
+        close(fd[0]);
+        char *message = "Hello My Name is Aayank\n";
+        write(fd[1], message, strlen(message));
+
+    }else{
+        close(fd[1]);
+        read(fd[0], buff, sizeof(buff));
+        printf("Reading data sent from parent to child: %s\n", buff);
+    }
+    return 0;
+}
+
+
+/*
+OUTPUT
+===========================================================================================================================
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P15$ ./a.out
+Reading data sent from parent to child: Hello My Name is Aayank
+===========================================================================================================================
+*/
