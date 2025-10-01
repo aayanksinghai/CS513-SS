56760c1d721b2573d196004dc5a5917c62db07a4
aayanksinghai <aayanksinghai02@gmail.com>
2025-10-01 16:14:07 +0530

    P20

---
+++ Hands-On-List2-Files/P20/20a.c
@@ -0,0 +1,45 @@
+/*
+===========================================================================================================================
+Name : 20a.c
+Author : Aayank Singhai
+Description : Write two programs so that both can communicate by FIFO -Use one way communication.
+Date: 1st October, 2025
+===========================================================================================================================
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// Writer Code
+int main() {
+    int fd;
+
+    // Create FIFO if it doesn't exist
+    mkfifo("myfifo", 0666);
+
+    // Open FIFO for writing
+    fd = open("myfifo", O_WRONLY);
+
+    char msg[] = "Hello from writer\n";
+    write(fd, msg, sizeof(msg));
+    printf("Writer: message %s", msg);
+
+    close(fd);
+    return 0;
+}
+
+
+/*
+OUTPUT
+===========================================================================================================================
+
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P20$ ./20a
+Writer: message Hello from writer
+
+===========================================================================================================================
+*/
---
+++ Hands-On-List2-Files/P20/20b.c
@@ -0,0 +1,50 @@
+/*
+===========================================================================================================================
+Name : 20b.c
+Author : Aayank Singhai
+Description : Write two programs so that both can communicate by FIFO -Use one way communication.
+Date: 1st October, 2025
+===========================================================================================================================
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+// Reader Code
+int main() {
+    int fd;
+
+    // Create FIFO if it doesn't exist
+    mkfifo("myfifo", 0666);
+
+    // Open FIFO for reading
+    fd = open("myfifo", O_RDONLY);
+
+    char buf[100];
+    int n = read(fd, buf, sizeof(buf) - 1);
+    if (n > 0) {
+        buf[n] = '\0';  // null terminate
+        printf("Reader: message %s", buf);
+    } else {
+        printf("Reader: no data received\n");
+    }
+
+    close(fd);
+    return 0;
+}
+
+/*
+OUTPUT
+===========================================================================================================================
+
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P20$ ./20b
+Reader: message Hello from writer
+
+===========================================================================================================================
+*/
