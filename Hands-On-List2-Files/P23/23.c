c9c5ba834c8d45df50c1865c1c08ce0eed6f94a3
aayanksinghai <aayanksinghai02@gmail.com>
2025-10-01 17:00:21 +0530

    P23

---
+++ Hands-On-List2-Files/P23/23.c
@@ -0,0 +1,30 @@
+/*
+===========================================================================================================================
+Name : 23.c
+Author : Aayank Singhai
+Description : Write a program to print the maximum number of files can be opened within a process and
+size of a pipe (circular buffer).
+Date: 1st October, 2025
+===========================================================================================================================
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+    printf("Max number of open files: %ld\n", sysconf(_SC_OPEN_MAX));
+    printf("Pipe size: %ld bytes\n", fpathconf(STDIN_FILENO, _PC_PIPE_BUF));
+
+    return 0;
+}
+
+/*
+OUTPUT
+===========================================================================================================================
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P23$ cc 23.c
+aayanksinghai@aayanksinghai-Vivobook-ASUSLaptop-K3605ZF-K3605ZF:~/CS513-SS/Hands-On-List2-Files/P23$ ./a.out
+Max number of open files: 1048576
+Pipe size: 4096 bytes
+===========================================================================================================================
+*/
